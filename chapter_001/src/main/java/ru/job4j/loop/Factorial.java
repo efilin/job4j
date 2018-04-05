2c94675e9a6a5c9fd6d23ec0d7fa95a07b56cf8e
efilin <2727fas@gmail.com>
2018-04-05 21:15:57 +0300

    4.2. Создать программу вычисляющую факториал.

---
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -0,0 +1,17 @@
+package ru.job4j.loop;
+
+/**
+ * @author  Eugeniy Filin (2727fas@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class Factorial {
+    public int calc(int n) {
+        int fac=1;
+        for (int i=1; i<=n; i++){
+            fac=fac*i;
+        }
+        return fac;
+    }
+}
