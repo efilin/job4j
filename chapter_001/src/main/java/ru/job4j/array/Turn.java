9bfe0044cbd2115b3563d5777a80c3c71543864f
efilin <2727fas@gmail.com>
2018-04-09 15:48:37 +0300

    Temp commit/

---
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -0,0 +1,16 @@
+package ru.job4j.array;
+
+/**
+ * @author  Eugeniy Filin (2727fas@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class Turn {
+    public int[] turn(int[] array) {
+        for (int i = 0; i != array.length) {
+
+        }
+        return array;
+    }
+}
