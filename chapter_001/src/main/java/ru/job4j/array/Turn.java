1ae6b7f1628d4e3498f45ae94f9c6d43ffb18dd9
efilin <2727fas@gmail.com>
2018-04-09 22:38:24 +0300

    5.2. Перевернуть массив.

--- chapter_001/src/main/java/ru/job4j/array/Turn.java
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -8,8 +8,18 @@ package ru.job4j.array;
 
 public class Turn {
     public int[] turn(int[] array) {
-        for (int i = 0; i != array.length) {
-
+        if (array.length % 2 == 0) {
+            for (int i = 0; i < (array.length) / 2; i++) {
+                int temp = array[i];
+                array[i] = array[(array.length - 1) - i];
+                array[(array.length - 1) - i] = temp;
+            }
+        } else {
+            for (int i = 0; i < (array.length - 1) / 2; i++) {
+                int temp = array[i];
+                array[i] = array[(array.length - 1) - i];
+                array[(array.length - 1) - i] = temp;
+            }
         }
         return array;
     }
--- chapter_001/src/test/java/ru/job4j/array/TurnTest.java
+++ chapter_001/src/test/java/ru/job4j/array/TurnTest.java
@@ -22,6 +22,10 @@ public class TurnTest {
 
     @Test
     public void whenTurnArrayWithOddAmountOfElementsThenTurnedArray() {
-        //напишите здесь тест, проверяющий переворот массива с нечётным числом элементов, например {1, 2, 3, 4, 5}.
+        Turn turner = new Turn();
+        int[] input = new int[] {1, 2, 3, 4, 5, 6};
+        int[] result = turner.turn(input);
+        int[] expect = new int[] {6, 5, 4, 3, 2, 1};
+        assertThat(result, is(expect));
     }
 }
