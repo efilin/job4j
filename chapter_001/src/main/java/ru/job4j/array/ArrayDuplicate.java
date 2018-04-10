70d2018a9f800f71ae1498f3196356f540698af7
efilin <2727fas@gmail.com>
2018-04-10 23:54:38 +0300

    5.6. Удаление дубликатов в массиве.v0.4

--- chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
@@ -11,8 +11,8 @@ import java.util.Arrays;
 public class ArrayDuplicate {
     public String[] remove(String[] array) {
         int k = 1;
-        for (int i = 0; i < array.length; i++) {
-            for (int j = i + 1; j < array.length; j++) {
+        for (int i = 0; i < array.length - k - 1; i++) {
+            for (int j = i + 1; j < array.length - k -1; j++) {
                 if (array[i].equals(array[j])) {
                     String temp = array[j];
                     array[j] = array[array.length - k];
@@ -20,7 +20,7 @@ public class ArrayDuplicate {
                     k++;
                 }
             }
-        } return Arrays.copyOf(array, array.length - k + 1);
+        } return Arrays.copyOf(array, array.length - k);
         //Перед обрезкой массива необходимо сгруппировать дубликаты в конце массива с помощью перестановок.
         //Метод должен убрать все дубликаты строк из массива;
         //Для обрезания массива надо использовать Arrays.copyOf метод;
