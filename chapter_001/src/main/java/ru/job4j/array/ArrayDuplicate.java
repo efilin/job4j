019a03c308797f691e3af4707cf4983d9341a846
efilin <2727fas@gmail.com>
2018-04-10 15:52:33 +0300

    5.6. Удаление дубликатов в массиве.v0.2

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
@@ -0,0 +1,16 @@
+package ru.job4j.array;
+
+import java.util.Arrays;
+
+/**
+ * @author  Eugeniy Filin (2727fas@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class ArrayDuplicate {
+    public String[] remove(String[] array){
+        //Метод должен убрать все дубликаты строк из массива;
+        //Для обрезания массива надо использовать Arrays.copyOf метод;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/ArrayDuplicateTest.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author  Eugeniy Filin (2727fas@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class ArrayDuplicateTest {
+    @Test
+    public void whenRemoveDuplicatesThenArrayWithoutDuplicate() {
+        //напишите здесь тест, проверяющий удаление дубликатов строк из массива строк.
+    }
+}
