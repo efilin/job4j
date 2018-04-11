295cd72aa50dd671ff6471f3dc99f6d4ce3f7a96
efilin <2727fas@gmail.com>
2018-04-11 23:24:45 +0300

    7.0 Проверка, что одно слово находится в другом слове.v0.4

---
+++ chapter_001/src/main/java/ru/job4j/array/CheckContain.java
@@ -0,0 +1,28 @@
+package ru.job4j.array;
+
+/**
+ * @author  Eugeniy Filin (2727fas@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class CheckContain {
+
+    public boolean contains(String origin, String sub) {
+        boolean result = false;
+        char[] value = sub.toCharArray();
+        char[] data = origin.toCharArray();
+        for (int i = 0; i < (data.length - value.length); i++) {
+            for (int j = 0; j < (value.length); ) {
+                if (data[i + j] == value[j]) {
+                    if (j == value.length - 1) {
+                        return result = true;
+                    }
+                    j++;
+                }
+            }
+        } return result;
+    }
+}
+
+
---
+++ chapter_001/src/test/java/ru/job4j/array/CheckContainTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author  Eugeniy Filin (2727fas@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class CheckContainTest {
+    @Test
+    public void whenContainThenTrue() {
+        CheckContain word = new CheckContain();
+        boolean result = word.contains("Helloworld", "low");
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenNotContainThenFalse() {
+        CheckContain word = new CheckContain();
+        boolean result = word.contains("Helloworld", "word");
+        assertThat(result, is(false));
+
+    }
+}
+
+
