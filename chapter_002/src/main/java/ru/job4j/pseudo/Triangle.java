b0b04c4b9209e3fce654ca07cf59c11967eb5b82
efilin <2727fas@gmail.com>
2018-04-23 16:04:49 +0300

    4.4 Используя шаблон проектирования - стратегию v1.1

--- chapter_002/src/main/java/ru/job4j/pseudo/Triangle.java
+++ chapter_002/src/main/java/ru/job4j/pseudo/Triangle.java
@@ -4,11 +4,11 @@ public class Triangle implements Shape {
     @Override
     public String draw() {
         StringBuilder pic = new StringBuilder();
-        pic.append("   +  ");
+        pic.append("   +   ");
         pic.append(System.lineSeparator());
-        pic.append("  + + ");
+        pic.append("  + +  ");
         pic.append(System.lineSeparator());
-        pic.append(" +   +");
+        pic.append(" +   + ");
         pic.append(System.lineSeparator());
         pic.append("+++++++");
         return pic.toString();
---
+++ chapter_002/src/test/java/ru/job4j/pseudo/TriangleTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.pseudo;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author  Eugeniy Filin (2727fas@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class TriangleTest {
+    @Test
+    public void whenDrawTriangle() {
+        Triangle triangle = new Triangle();
+        assertThat(
+                triangle.draw(),
+                is(
+                        new StringBuilder()
+                                .append("   +   ")
+                                .append(System.lineSeparator())
+                                .append("  + +  ")
+                                .append(System.lineSeparator())
+                                .append(" +   + ")
+                                .append(System.lineSeparator())
+                                .append("+++++++")
+                                .toString()
+                )
+        );
+    }
+}
