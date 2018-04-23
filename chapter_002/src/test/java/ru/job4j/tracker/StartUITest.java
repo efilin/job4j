71ff2bfe27c1a83e6074ad736bd621e0cc363e83
efilin <2727fas@gmail.com>
2018-04-23 23:59:10 +0300

    6. Доработать тесты в StartUITest проверяющие вывод данных в консоль.v0.2

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,12 +1,37 @@
 package ru.job4j.tracker;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 
 
 public class StartUITest {
+
+    // поле содержит дефолтный вывод в консоль.
+    private final PrintStream stdout = System.out;
+    // буфер для результата.
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method");
+    }
+
+
     @Test
     public void whenUserAddItemThenTrackerHasNewItemWithSameName() {
         Tracker tracker = new Tracker();
@@ -46,6 +71,42 @@ public class StartUITest {
         Input input = new StubInput(new String[]{"3", itemOne.getId(), "6"});
         new StartUI(input, tracker).init();
         assertThat(tracker.findAll()[0].getName(), is("test name2"));
+
+
+    }
+    @Test
+    public void whenShowAll() {
+        Tracker tracker = new Tracker();
+        Item itemOne = tracker.add(new Item("test name", "desc", 3));
+        Item itemTwo = tracker.add(new Item("test name2", "desc2", 4));
+        Input input = new StubInput(new String[]{"1", "6"});
+        new StartUI(input, tracker).init();
+        assertThat(
+                new String(out.toByteArray()),
+                is(
+                        new StringBuilder()
+                                .append("test name")
+                                .append(System.lineSeparator())
+                                .append("desc")
+                                .append(System.lineSeparator())
+                                .append("test name2")
+                                .append(System.lineSeparator())
+                                .append("desc2")
+                                .append(System.lineSeparator())
+                                .toString()
+                )
+        );
+
+    }
+
+    @Test
+    public void whenFindId() {
+
+
+    }
+
+    @Test
+    public  void whenFindName() {
 
 
     }
