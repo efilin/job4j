9504c6d4962364b83cebbf27f9e5bf9453d11b0c
efilin <2727fas@gmail.com>
2018-06-19 16:42:12 +0300

    5.3.1. Создать динамический список на базе массива.

---
+++ chapter_004/src/main/java/ru/job4j/list/DynamicArray.java
@@ -0,0 +1,29 @@
+package ru.job4j.list;
+
+import java.util.Iterator;
+
+public class DynamicArray <E> implements Iterable<E> {
+
+    private Object[] container = new Object[10];
+    private int size = 0;
+    protected int modCount = 0;
+
+
+    void
+
+
+
+    void add (E e){
+
+    }
+
+
+    E get(int index) {
+        return null;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+}
