52c54dceb565bc01e28ab669f6ac4eeec7799ba8
efilin <2727fas@gmail.com>
2018-06-20 13:43:00 +0300

    5.3.1. Создать динамический список на базе массива.v1.0

--- chapter_004/src/main/java/ru/job4j/list/DynamicArray.java
+++ chapter_004/src/main/java/ru/job4j/list/DynamicArray.java
@@ -1,29 +1,68 @@
 package ru.job4j.list;
 
+import java.util.Arrays;
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
-public class DynamicArray <E> implements Iterable<E> {
+public class DynamicArray<E> implements Iterable<E> {
 
     private Object[] container = new Object[10];
     private int size = 0;
     protected int modCount = 0;
 
 
-    void
+    void checkCapacity() {
+        if (size == container.length) {
+            modCount++;
+            container = Arrays.copyOf(container, container.length + container.length / 2);
+        }
+    }
 
 
-
-    void add (E e){
-
+    void add(E e) {
+        checkCapacity();
+        container[size++] = e;
     }
 
 
     E get(int index) {
-        return null;
+        return (E) container[index];
     }
+
 
     @Override
     public Iterator<E> iterator() {
-        return null;
+        return new Iterator<E>() {
+            int index = 0;
+            int expectedModCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+                return index < size;
+            }
+
+            @Override
+            public E next() {
+                checkForComodification();
+                int i = index;
+                if (i >= size) {
+                    throw new NoSuchElementException();
+                }
+                Object[] elementData = DynamicArray.this.container;
+                if (i >= elementData.length) {
+                    throw new ConcurrentModificationException();
+                }
+                index = i + 1;
+                return (E) elementData[i];
+            }
+
+            final void checkForComodification() {
+                if (modCount != expectedModCount) {
+                    throw new ConcurrentModificationException();
+                }
+            }
+        };
+
     }
 }
---
+++ chapter_004/src/test/java/ru/job4j/list/DynamicArrayTest.java
@@ -0,0 +1,51 @@
+package ru.job4j.list;
+
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class DynamicArrayTest {
+
+    @Test
+    public void whenAddThenGet() {
+        DynamicArray<Integer> dynArray = new DynamicArray<>();
+        dynArray.add(4);
+        dynArray.add(5);
+        assertThat(dynArray.get(1), is(5));
+    }
+
+    @Test
+    public void whenMoreThanTenElementsAndArrayGrows() {
+        DynamicArray<Integer> dynArray = new DynamicArray<>();
+        dynArray.add(3);
+        dynArray.add(4);
+        dynArray.add(5);
+        dynArray.add(6);
+        dynArray.add(7);
+        dynArray.add(8);
+        dynArray.add(9);
+        dynArray.add(10);
+        dynArray.add(11);
+        dynArray.add(12);
+        dynArray.add(13);
+        assertThat(dynArray.get(10), is(13));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenIterateTillEndOfElements() {
+        DynamicArray<Integer> dynArray = new DynamicArray<>();
+        dynArray.add(3);
+        dynArray.add(4);
+        Iterator<Integer> it = dynArray.iterator();
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(3));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(4));
+        assertThat(it.hasNext(), is(false));
+        it.next();
+    }
+}
