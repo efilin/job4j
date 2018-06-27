32bf416e4fbf7240d9585cb4d8d8485871c30d32
efilin <2727fas@gmail.com>
2018-06-27 16:41:41 +0300

    3. Реализовать коллекцию типа Set на базе хэш-таблицы v0.2

---
+++ chapter_004/src/test/java/ru/job4j/set/SimpleHashSet.java
@@ -0,0 +1,43 @@
+package ru.job4j.set;
+
+import ru.job4j.list.DynamicArray;
+
+public class SimpleHashSet<E> {
+    private int arrayLength = 10;
+    DynamicArray<E> [] simpleList = new DynamicArray[arrayLength];
+
+    public SimpleHashSet() {
+        for (int i =0; i < arrayLength; i++) {
+            simpleList[i] = new DynamicArray<>();
+        }
+    }
+
+    boolean add (E e){
+        if (!contains(e)) {
+            simpleList[hash(e)].add(e);
+            return true;
+        }
+        return false;
+    }
+
+
+
+    boolean contains (E e) {
+        return e.equals(simpleList[hash(e)]);
+    }
+
+    boolean remove (E e) {
+        if (!contains(e)) {
+            return false;
+        }
+        return false;
+    }
+
+    private int hash(E e) {
+        return e.hashCode() % arrayLength;
+    }
+
+
+
+
+}
