05fe01ab7ca173680d2ae86b2bffd32344aa3e7c
efilin <2727fas@gmail.com>
2018-07-12 15:18:49 +0300

    1. Создать элементарную структуру дерева

---
+++ chapter_004/src/main/java/ru/job4j/tree/Tree.java
@@ -0,0 +1,67 @@
+package ru.job4j.tree;
+
+import java.util.*;
+
+public class Tree<E extends Comparable<E>> implements SimpleTree<E> {
+
+    private Node<E> root;
+
+    public Tree(E e) {
+        this.root = new Node<>(e);
+    }
+
+    @Override
+    public boolean add(E parent, E child) {
+        if (findBy(child) == null) {
+            return false;
+        }
+        findBy(parent).get().add(new Node<>(child));
+        return true;
+    }
+
+
+    @Override
+    public Optional<Node<E>> findBy(E value) {
+        Optional<Node<E>> rsl = Optional.empty();
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> el = data.poll();
+            if (el.eqValue(value)) {
+                rsl = Optional.of(el);
+                break;
+            }
+            for (Node<E> child : el.leaves()) {
+                data.offer(child);
+            }
+        }
+        return rsl;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+
+            Queue<Node<E>> data = new LinkedList<>();
+            boolean r = data.offer(Tree.this.root);
+
+
+            @Override
+            public boolean hasNext() {
+                return !data.isEmpty();
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                Node<E> result = data.poll();
+                for (Node<E> element : result.leaves()) {
+                    data.offer(element);
+                }
+                return result.getValue();
+            }
+        };
+    }
+}
