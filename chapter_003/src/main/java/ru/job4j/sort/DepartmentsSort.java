32f3146b7cfe6130730081b3ee73c1faaeda7a1f
efilin <2727fas@gmail.com>
2018-07-17 23:52:13 +0300

    5.4 Отсортировать департаменты v0.6

--- chapter_003/src/main/java/ru/job4j/sort/DepartmentsSort.java
+++ chapter_003/src/main/java/ru/job4j/sort/DepartmentsSort.java
@@ -1,27 +1,45 @@
 package ru.job4j.sort;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.NavigableSet;
+import java.util.TreeSet;
+
 public class DepartmentsSort {
 
     private String[] departments;
 
 
     public String[] ascendingSort(String[] departments) {
+        ArrayList<String> result  = getMissedDep(departments);
+        TreeSet<String> treeSet = new TreeSet<>(result);
 
-        String[] result = new String[departments.length + getMissedDep(departments).length];
+        //to do
 
-
-        return result;
+        return treeSet.toArray(new String[0]);
     }
 
     public String[] descendingSort(String[] departments) {
-        String[] result = new String[departments.length + getMissedDep(departments).length];
-        return result;
+        ArrayList<String> result  = getMissedDep(departments);
+        TreeSet<String> treeSet = new TreeSet<>(result);
+        NavigableSet<String> sortedSet = treeSet.descendingSet();
+        return sortedSet.toArray(new String[0]);
     }
 
 
-    public String[] getMissedDep(String[] departments) {
-
-        String[] result = new String[departments.length];
+    public ArrayList<String> getMissedDep(String[] departments) {
+        ArrayList<String> result = new ArrayList<String>(Arrays.asList(departments));
+        ArrayList<String> missedElements = new ArrayList<>();
+        for (String el: result) {
+            for (int index = 0; index < el.length() - 1; index++) {
+                if (el.substring(index, index + 1).equals("\\")) {
+                    if (!result.contains(el.substring(0,index))) {
+                        missedElements.add(el.substring(0, index));
+                    }
+                }
+            }
+        }
+        result.addAll(missedElements);
         return result;
     }
 
