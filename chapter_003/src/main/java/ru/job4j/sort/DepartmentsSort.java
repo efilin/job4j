a3a73200c0413f5b89c02f41f160e048681c0765
efilin <2727fas@gmail.com>
2018-08-20 13:35:51 +0300

    5.4 Отсортировать департаменты v1.1

--- chapter_003/src/main/java/ru/job4j/sort/DepartmentsSort.java
+++ chapter_003/src/main/java/ru/job4j/sort/DepartmentsSort.java
@@ -1,18 +1,18 @@
 package ru.job4j.sort;
 
+
 import java.util.*;
 
 public class DepartmentsSort {
 
 
     public String[] ascendingSort(String[] departments) {
-        ArrayList<String> result = getMissedDep(departments);
-        TreeSet<String> treeSet = new TreeSet<>(result);
+        Set<String> treeSet = getMissedDep(departments);
         return treeSet.toArray(new String[0]);
     }
 
     public String[] descendingSort(String[] departments) {
-        ArrayList<String> result = getMissedDep(departments);
+        ArrayList<String> result = new ArrayList<>(getMissedDep(departments));
         result.sort(new Comparator<String>() {
                         @Override
                         public int compare(String o1, String o2) {
@@ -33,20 +33,15 @@ public class DepartmentsSort {
         return result.toArray(new String[0]);
     }
 
-
-    public ArrayList<String> getMissedDep(String[] departments) {
-        ArrayList<String> result = new ArrayList<String>(Arrays.asList(departments));
-        ArrayList<String> missedElements = new ArrayList<>();
-        for (String el : result) {
+    public Set<String> getMissedDep(String[] departments) {
+        Set<String> result = new TreeSet<>(Arrays.asList(departments));
+        for (String el : departments) {
             for (int index = 0; index < el.length() - 1; index++) {
                 if (el.substring(index, index + 1).equals("\\")) {
-                    if (!result.contains(el.substring(0, index)) && !missedElements.contains(el.substring(0, index))) {
-                        missedElements.add(el.substring(0, index));
-                    }
+                    result.add(el.substring(0, index));
                 }
             }
         }
-        result.addAll(missedElements);
         return result;
     }
 }
