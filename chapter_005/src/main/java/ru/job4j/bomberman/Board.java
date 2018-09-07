0b8cfea3a0c5e5325856dae01a01d5dc2329c641
efilin <2727fas@gmail.com>
2018-09-07 16:22:06 +0300

    Игра Бомбермен v0.1

---
+++ chapter_005/src/main/java/ru/job4j/bomberman/Board.java
@@ -0,0 +1,46 @@
+package ru.job4j.bomberman;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Board {
+
+    private final int size = 8;
+
+    ReentrantLock[][] board = new ReentrantLock[size][size];
+
+
+    public boolean move(Cell source, Cell dest) {
+        board[source.getX()][source.getY()].lock();  // block until condition holds
+        try {
+            board[dest.getX()][dest.getY()] = board[source.getX()][source.getY()];
+            board[source.getX()][source.getY()] = null;
+            // ... method body
+        } finally {
+            board[source.getX()][source.getY()].unlock();
+        }
+
+        return true;
+    }
+
+
+    Thread heroOne = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            board[0][0].lock();
+            while (true) {
+
+
+            }
+
+        }
+    });
+
+    Thread heroTwo = new Thread(new Runnable() {
+        @Override
+        public void run() {
+
+        }
+    });
+
+
+}
---
+++ chapter_005/src/main/java/ru/job4j/bomberman/Cell.java
@@ -0,0 +1,20 @@
+package ru.job4j.bomberman;
+
+public class Cell {
+
+    private final int x;
+    private final int y;
+
+    public Cell(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
