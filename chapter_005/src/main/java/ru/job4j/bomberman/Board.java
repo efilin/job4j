ae43edc88285803e99021273ba96311914acd2d8
efilin <2727fas@gmail.com>
2018-09-18 12:45:42 +0300

    Игра Бомбермен. Первый этап.

--- chapter_005/src/main/java/ru/job4j/bomberman/Board.java
+++ chapter_005/src/main/java/ru/job4j/bomberman/Board.java
@@ -1,43 +1,96 @@
 package ru.job4j.bomberman;
 
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantLock;
+
 
 public class Board {
 
     private final int size = 8;
 
-    ReentrantLock[][] board = new ReentrantLock[size][size];
+    private final ReentrantLock[][] board = new ReentrantLock[size][size];
+    Random random = new Random();
+
+    private ReentrantLock boardCell(Cell cell) {
+        return this.board[cell.getX()][cell.getY()];
+    }
 
 
+    /**
+     * Метод move() пробует залочить новую ячейку в течении 500мс,
+     * если нет выдает false;
+     *
+     * @param source cell
+     * @return
+     */
     public boolean move(Cell source, Cell dest) {
-        board[source.getX()][source.getY()].lock();  // block until condition holds
         try {
-            board[dest.getX()][dest.getY()] = board[source.getX()][source.getY()];
-            board[source.getX()][source.getY()] = null;
-            // ... method body
-        } finally {
-            board[source.getX()][source.getY()].unlock();
+            return boardCell(dest).tryLock(500, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+        return false;
+    }
 
-        return true;
+    private Cell changePosition(Cell source) {
+        Cell result;
+        do {
+            result = new Cell(random.nextInt(size), random.nextInt(size));
+        }
+        while (!result.equals(source));
+        return result;
+    }
+
+    private Cell startPosition() {
+        Cell result;
+        do {
+            result = new Cell(random.nextInt(size), random.nextInt(size));
+        }
+        while (boardCell(result).tryLock());
+        return result;
     }
 
 
     Thread heroOne = new Thread(new Runnable() {
         @Override
         public void run() {
-            board[0][0].lock();
-            /*while (true) {
-
-
-            }*/
-
+            Cell startCell = startPosition();
+            Cell nextCell;
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    nextCell = changePosition(startCell);
+                    if (move(startCell, nextCell)) {
+                        boardCell(startCell).unlock();
+                        startCell = nextCell;
+                        Thread.sleep(1000);
+                    }
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    e.printStackTrace();
+                }
+            }
         }
     });
 
     Thread heroTwo = new Thread(new Runnable() {
         @Override
         public void run() {
+            Cell startCell = startPosition();
+            Cell nextCell;
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    nextCell = changePosition(startCell);
+                    if (move(startCell, nextCell)) {
+                        boardCell(startCell).unlock();
+                        startCell = nextCell;
+                        Thread.sleep(1000);
+                    }
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    e.printStackTrace();
+                }
+            }
 
         }
     });
