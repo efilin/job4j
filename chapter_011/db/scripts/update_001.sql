cdf623778c910ad0867fb196216c2d4b054e393c
efilin <2727fas@gmail.com>
2021-01-29 12:33:45 +0300

    fixed CI build failure

--- chapter_011/db/scripts/update_001.sql
+++ chapter_011/db/scripts/update_001.sql
@@ -1,4 +1,4 @@
-create table users_storage(
+create table if not exists  users_storage(
   id SERIAL PRIMARY KEY,
   name VARCHAR(50)
 )
--- chapter_011/src/test/java/ru/job4j/beanexamples/UserTest.java
+++ chapter_011/src/test/java/ru/job4j/beanexamples/UserTest.java
@@ -13,21 +13,21 @@ public class UserTest {
     public void whenCreateBeanWithXmlConfigurationWithConstructorInjection() {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
         User userOne = context.getBean("userOne", User.class);
-        assertThat(userOne.getName(), is("Sasha"));
+        assertEquals(userOne.getName(), "Sasha");
     }
 
     @Test
     public void whenCreateBeanWithXmlConfigurationWithSetterInjection() {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
         User userOne = context.getBean("userTwo", User.class);
-        assertThat(userOne.getName(), is("Kolya"));
+        assertEquals(userOne.getName(), "Kolya");
     }
 
     @Test
     public void whenCreateBeanWithAnnotationConfigurationWithConstructorInjection() {
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         User userThree = context.getBean(User.class);
-        assertThat(userThree.getName(), is("Masha"));
+        assertEquals(userThree.getName(), "Masha");
     }
 
 }
